4c7aa2087cb82a568878422581d9a8838af04db7
Subir Sutradhar <ssutradhar35@gmail.com>
2026-09-12 21:13:46 +0530

    feat: add test for update record

--- practicals/06-dictionary-record-manager/tests/test_record_manager.py
+++ practicals/06-dictionary-record-manager/tests/test_record_manager.py
@@ -1,3 +1,4 @@
+import pytest
 from record_manager import RecordManager
 
 
@@ -7,4 +8,19 @@ def test_add_record():
         "001", "Subir", "ss@aol.com", "Developer", True
         )
     assert result == "Record with user_id 001 added."
-    # user.get_record("001")
+    record = user.get_record("001")
+    assert record == {'user_id': '001', 'name': 'Subir', 'email': 'ss@aol.com', 'department': 'Developer', 'active': True}
+
+
+def test_update_record():
+    user = RecordManager()
+    user.add_record(
+        "001", "Subir", "ss@aol.com", "Developer", True
+        )
+    update = user.update_records(
+    user_id="001",
+    name="gray",
+    email="foo@spam.com"
+    )
+    assert update == "Record with ID 001 updated."
+
